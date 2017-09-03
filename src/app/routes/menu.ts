d4419174b29aa8411b179f544d255b694b2082e1
Shital More <shitalmore94@outlook.com>
2017-09-04 01:08:52 +0530

    initial commit

---
+++ src/app/routes/menu.ts
@@ -0,0 +1,25 @@
+
+const Dashboard = {
+    text: 'Dashboard',
+    link: '/dashboard',
+    icon: 'fa fa-table',
+    active: true,
+    activeLinks: ['/dashboard']
+};
+
+const headingMain = {
+    text: 'Main Navigation',
+    heading: true
+};
+
+
+export const menu = [
+    Dashboard
+
+];
+
+export const navigationMenu = [
+    Dashboard
+];
+
+
---
+++ src/app/routes/routes.ts
@@ -0,0 +1,19 @@
+import { LayoutComponent } from '../layout/layout.component';
+
+export const routes = [
+
+    {
+        path: '',
+        component: LayoutComponent,
+        children: [
+            { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+            { path: 'dashboard', loadChildren: './dashboard/dashboard.module#DashboardModule' },
+     
+        ]
+    },
+
+
+    // Not found
+    { path: '**', redirectTo: 'dashboard' }
+
+];
